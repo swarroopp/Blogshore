62721e4dccc0a5d4860463813ff5360b199c7f4b
swarroopp <swaroopmallid7777@example.com>
2025-03-27 14:27:14 +0530

    changed footer

--- draft-blogapp-main/client/src/components/common/Footer.jsx
+++ draft-blogapp-main/client/src/components/common/Footer.jsx
@@ -43,11 +43,11 @@ const Footer = () => {
       </div>
       
       {/* Wave decoration */}
-      <div className="footer-wave">
+      {/* <div className="footer-wave">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
           <path fill="currentColor" fillOpacity="0.2" d="M0,96L48,112C96,128,192,160,288,160C384,160,480,128,576,128C672,128,768,160,864,170.7C960,181,1056,171,1152,149.3C1248,128,1344,96,1392,80L1440,64L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
         </svg>
-      </div>
+      </div> */}
     </footer>
   );
 };
